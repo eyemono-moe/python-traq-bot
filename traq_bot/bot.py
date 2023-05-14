decde2eb210f750b2cfe99261908b6f876d65329
detteiu8383 <ysdr83@gmail.com>
2023-05-15 06:31:01 +0900

    format

--- traq_bot/bot.py
+++ traq_bot/bot.py
@@ -1,8 +1,8 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-from typing import Callable, Dict, List, Optional, Union
-from inspect import _empty, signature
-import os
 import json
+import os
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+from inspect import _empty, signature
+from typing import Callable, Dict, List, Optional, Union
 
 
 class TraqBot:
@@ -20,8 +20,8 @@ class TraqBot:
         print(data)
 
     # Start your app
-    if __name__ == "__main__":  
-        bot.run()  
+    if __name__ == "__main__":
+        bot.run()
 
     Args:
         verification_token (str): Your bot token.
@@ -48,7 +48,7 @@ class TraqBot:
             "USER_CREATED": [],
             "STAMP_CREATED": [],
             "TAG_ADDED": [],
-            "TAG_REMOVED": []
+            "TAG_REMOVED": [],
         }
 
     def run(self, port: int = 8080) -> None:
@@ -63,19 +63,11 @@ class TraqBot:
     def _handle_event(self, event: str, data: Dict) -> Dict:
         if event == "PING":
             # 204返す
-            resp = {
-                "status": 204,
-                "headers": {},
-                "body": ""
-            }
+            resp = {"status": 204, "headers": {}, "body": ""}
             return resp
 
         if len(self._handlers[event]) == 0:
-            resp = {
-                "status": 501,
-                "headers": {},
-                "body": ""
-            }
+            resp = {"status": 501, "headers": {}, "body": ""}
             return resp
 
         try:
@@ -86,31 +78,29 @@ class TraqBot:
                 if len(sig.parameters) == 0:
                     func()
                 # もし引数を1つ持っていたら`data`を与えて関数を実行する
-                elif ((len(sig.parameters) == 1) and (next(iter(sig.parameters.values())).annotation in [Dict, _empty])):
+                elif (len(sig.parameters) == 1) and (
+                    next(iter(sig.parameters.values())).annotation in [Dict, _empty]
+                ):
                     func(data)
                 else:
                     raise Exception("イベントハンドラは0または1つの辞書型の引数を取るようにしてください")
             # ここで204返す
-            resp = {
-                "status": 204,
-                "headers": {},
-                "body": ""
-            }
+            resp = {"status": 204, "headers": {}, "body": ""}
             return resp
         except Exception as e:
             print(e)
             # ここで500とか返す
-            resp = {
-                "status": 500,
-                "headers": {},
-                "body": ""
-            }
+            resp = {"status": 500, "headers": {}, "body": ""}
             return resp
 
-    def _register_function(self, func: Callable[[Optional[Dict]], None], event: str) -> None:
+    def _register_function(
+        self, func: Callable[[Optional[Dict]], None], event: str
+    ) -> None:
         self._handlers[event].append(func)
 
-    def ping(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def ping(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register ping event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -119,7 +109,9 @@ class TraqBot:
         self._register_function(func, "PING")
         return func
 
-    def joined(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def joined(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register joined event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -130,7 +122,9 @@ class TraqBot:
         self._register_function(func, "JOINED")
         return func
 
-    def left(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def left(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register left event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -141,7 +135,9 @@ class TraqBot:
         self._register_function(func, "LEFT")
         return func
 
-    def message_created(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def message_created(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register message created event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -152,7 +148,9 @@ class TraqBot:
         self._register_function(func, "MESSAGE_CREATED")
         return func
 
-    def message_deleted(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def message_deleted(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register joined message deleted handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -163,7 +161,9 @@ class TraqBot:
         self._register_function(func, "MESSAGE_DELETED")
         return func
 
-    def message_updated(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def message_updated(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register message updated event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -174,7 +174,9 @@ class TraqBot:
         self._register_function(func, "MESSAGE_UPDATED")
         return func
 
-    def direct_message_created(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def direct_message_created(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register direct message created event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -185,7 +187,9 @@ class TraqBot:
         self._register_function(func, "DIRECT_MESSAGE_CREATED")
         return func
 
-    def direct_message_deleted(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def direct_message_deleted(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register direct message deleted event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -196,7 +200,9 @@ class TraqBot:
         self._register_function(func, "DIRECT_MESSAGE_DELETED")
         return func
 
-    def direct_message_updated(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def direct_message_updated(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register direct message updated event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -207,7 +213,9 @@ class TraqBot:
         self._register_function(func, "DIRECT_MESSAGE_UPDATED")
         return func
 
-    def bot_message_stamps_updated(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def bot_message_stamps_updated(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register bot message stamps updated event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -218,7 +226,9 @@ class TraqBot:
         self._register_function(func, "BOT_MESSAGE_STAMPS_UPDATED")
         return func
 
-    def channel_created(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def channel_created(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register channel created event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -229,7 +239,9 @@ class TraqBot:
         self._register_function(func, "CHANNEL_CREATED")
         return func
 
-    def channel_topic_changed(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def channel_topic_changed(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register channel topic changed event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -240,7 +252,9 @@ class TraqBot:
         self._register_function(func, "CHANNEL_TOPIC_CHANGED")
         return func
 
-    def user_created(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def user_created(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register user created event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -251,7 +265,9 @@ class TraqBot:
         self._register_function(func, "USER_CREATED")
         return func
 
-    def stamp_created(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def stamp_created(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register stamp created event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -262,7 +278,9 @@ class TraqBot:
         self._register_function(func, "STAMP_CREATED")
         return func
 
-    def tag_added(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def tag_added(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register tag added event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -273,7 +291,9 @@ class TraqBot:
         self._register_function(func, "TAG_ADDED")
         return func
 
-    def tag_removed(self, func: Callable[[Optional[Dict]], None]) -> Callable[[Optional[Dict]], None]:
+    def tag_removed(
+        self, func: Callable[[Optional[Dict]], None]
+    ) -> Callable[[Optional[Dict]], None]:
         """Register tag removed event handler. This method can be used as either a decorator or a method.
 
         Args:
@@ -293,17 +313,14 @@ class TraqBotServer:
 
         class TraqBotHandler(SimpleHTTPRequestHandler):
             def do_POST(self) -> None:
-                content_length: int = int(
-                    self.headers.get('Content-Length')) or 0
+                content_length: int = int(self.headers.get("Content-Length") or 0)
 
-                verification_token: Optional[str] = self.headers.get(
-                    'X-TRAQ-BOT-TOKEN'
-                )
+                verification_token: Optional[str] = self.headers.get("X-TRAQ-BOT-TOKEN")
                 if verification_token is None:
                     print("No X-TRAQ-BOT-TOKEN")
                     self._send_response(401, {}, "")
                     return
- 
+
                 if verification_token != _bot._verification_token:
                     print("Verification token mismatch")
                     self._send_response(401, {}, "")
@@ -320,7 +337,7 @@ class TraqBotServer:
                     data: Dict = {}
                 else:
                     try:
-                        data: Dict = json.loads(request_body.decode('utf-8'))
+                        data: Dict = json.loads(request_body.decode("utf-8"))
                     except json.JSONDecodeError:
                         print("JSONDecodeError")
                         self._send_response(400, {}, "")
@@ -329,18 +346,19 @@ class TraqBotServer:
                 bot_resp = _bot._handle_event(event, data)
                 self._send_bot_response(bot_resp)
 
-            def _send_response(self, status: int, headers: Dict, body: Union[str, Dict]) -> None:
+            def _send_response(
+                self, status: int, headers: Dict, body: Union[str, Dict]
+            ) -> None:
                 self.send_response(status)
 
-                response_body = body if isinstance(
-                    body, str) else json.dumps(body)
-                response_body.encode('utf-8')
-                byte_body = response_body.encode('utf-8')
+                response_body = body if isinstance(body, str) else json.dumps(body)
+                response_body.encode("utf-8")
+                byte_body = response_body.encode("utf-8")
 
                 for k, vs in headers.items():
                     for v in vs:
                         self.send_header(k, v)
-                self.send_header('Content-Length', str(len(byte_body)))
+                self.send_header("Content-Length", str(len(byte_body)))
                 self.end_headers()
                 self.wfile.write(byte_body)
 
@@ -348,11 +366,10 @@ class TraqBotServer:
                 self._send_response(
                     status=bot_resp["status"],
                     headers=bot_resp["headers"],
-                    body=bot_resp["body"]
+                    body=bot_resp["body"],
                 )
 
-        self._server: HTTPServer = HTTPServer(
-            ("0.0.0.0", self._port), TraqBotHandler)
+        self._server: HTTPServer = HTTPServer(("0.0.0.0", self._port), TraqBotHandler)
 
     def run(self) -> None:
         try:
